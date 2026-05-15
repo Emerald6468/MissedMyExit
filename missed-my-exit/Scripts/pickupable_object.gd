c51a08cf2e39bbccf324e5626d8ccf1a67d9d1e9
Emerald6468 <139382641+Emerald6468@users.noreply.github.com>
2026-05-14 19:16:02 -0500

    pickup finished

--- missed-my-exit/Scripts/pickupable_object.gd
+++ missed-my-exit/Scripts/pickupable_object.gd
@@ -1,6 +1,10 @@
 extends Area3D
 
+@export var ObjectType: String
+
 var player_nearby = false
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var body_list = get_overlapping_bodies()
@@ -17,5 +21,9 @@ func _process(delta: float) -> void:
 		player_nearby = false
 	if player_nearby and Global.JustPickedUp:
 		Global.JustPickedUp = false
-		Global.HasRock = true
-		queue_free()
+		if ObjectType != "":
+			#Have all of the different objects it can be here
+			match ObjectType:
+				"Rock":
+					Global.HasRock = true
+			queue_free()
