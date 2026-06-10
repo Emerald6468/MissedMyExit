009af69f2f2fe2f4a13541cb75ead496297558e8
Emerald6468 <139382641+Emerald6468@users.noreply.github.com>
2026-06-09 19:34:49 -0500

    car in loop

--- missed-my-exit/Scripts/Global.gd
+++ missed-my-exit/Scripts/Global.gd
@@ -2,7 +2,7 @@ extends Node
 #This is our global node make sure to be careful with resetting stuff
 
 #Switching Controls
-var OnFoot = true
+var OnFoot = false
 var JustSwitched = false
 var NearDoor = false
 
@@ -23,7 +23,7 @@ var PlayerPosition: Vector3
 
 #make sure to add every var to reset
 func reset_vars():
-	OnFoot = true
+	OnFoot = false
 	JustSwitched = false
 	NearDoor = false
 	NearPickup = false
